5d91db3413d29de03777e78b9423ffffca36ee3e
saff-it <saff_it@ymail.com>
2022-07-04 16:25:36 +0200

    Bonus attempting

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const app = new Vue (
 
         data: {
             titleTextInH1: '',
+            imageToHtml: 'https://www.kodami.it/lo-shiba-inu-elegante-e-coraggioso-guardiano-giapponese/',
         },
 
         methods: {
