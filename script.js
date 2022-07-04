f4c6ce4c988f5459f3143a9feb609841ef82da16
saff-it <saff_it@ymail.com>
2022-07-04 16:46:59 +0200

    click function testing

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const app = new Vue (
         data: {
             titleTextInH1: '',
             imageToHtml: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Foto_Shiba_Inu_.jpg/1200px-Foto_Shiba_Inu_.jpg',
+            imageAfterClick: '',
         },
 
         methods: {
-            funzione: function() {
-
+            clickToDisplayImg: function() {
+                this.imageAfterClick = this.imageToHtml;
+                return;
             },
         }
     }
