734d55ab2b870599b04a5f83d429b6278fc20d84
saff-it <saff_it@ymail.com>
2022-07-04 15:13:47 +0200

    vue set up

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const app = new Vue (
+    {
+        el:'#root',
+
+        data: {
+            variabile: ''
+        },
+
+        methods: {
+            funzione: function() {
+
+            },
+        }
+    }
+
+);
